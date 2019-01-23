140a4edf9f9be29588e5d237ef07706b301cb74d
Ideilson Cisne <ideilson@rubert.com.br>
2019-01-23 18:58:14 -0300

    Added method of query and change

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,21 @@
 # encoding: utf-8
 class Book
-  def initialize(author, isbn = "1", number_of_pages)
+  def initialize(author, isbn = "1", number_of_pages, price)
     @author = author
     @isbn = isbn
     @number_of_pages = number_of_pages
+    @price = price
   end
 
   def to_s
     "Author: #{@author}, ISBN: #{@isbn}, Number of Pages: #{@number_of_pages}"
+  end
+
+  def price # método que consulta a variável de instância
+    @price
+  end
+
+  def price=(price) # método que altera o valor da variável de instância
+    @price = price
   end
 end
